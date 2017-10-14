a1a7ba893f454959c062d366a28bac895c1d9dbc
Yiannis Tsenes <itsenes@indice.gr>
2017-10-14 04:03:13 +0300

    tslint fixes

--- src/app/layout/shell/shell.component.ts
+++ src/app/layout/shell/shell.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, NavigationStart,ActivatedRoute } from '@angular/router';
+import { Router, NavigationStart, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-shell',
