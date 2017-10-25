e89d69e49d611994cec51d48a4ed36f21f4c552b
Yiannis Tsenes <itsenes@indice.gr>
2017-10-25 04:44:12 +0300

    account settings ui work

--- src/app/features/account-settings/account-settings.component.ts
+++ src/app/features/account-settings/account-settings.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
-
+import { AppStateService } from '../../services/app-state.service';
 @Component({
   selector: 'app-account-settings',
   templateUrl: './account-settings.component.html',
-  styleUrls: ['./account-settings.component.css']
+  styleUrls: ['./account-settings.component.scss']
 })
 export class AccountSettingsComponent implements OnInit {
   user = null;
-  constructor(private authService: AuthService) {
+  subscriptions = null;
+  constructor(private appState: AppStateService, private authService: AuthService) {
     this.user = authService.currentUser().profile;
+    this.subscriptions = appState.subscriptions;
   }
 
   ngOnInit() {
--- src/app/services/app-state.service.ts
+++ src/app/services/app-state.service.ts
@@ -1,10 +1,9 @@
 import { Injectable } from '@angular/core';
-
+// a simple singleton to maintain app state for now, other patterns could be applied
 @Injectable()
 export class AppStateService {
   public subscriptions = [];
   public document_types = [];
   public selected_subscription: any = null;
   constructor() { }
-
 }
