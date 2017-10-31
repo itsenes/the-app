73f6b4371ee14d48fc93db1168b0b90b0ec13b10
Yiannis Tsenes <itsenes@indice.gr>
2017-10-31 12:12:08 +0200

    trying to style mat-form-input - before checking for outdated libs

--- src/app/features/forms/company-form/company-form.component.ts
+++ src/app/features/forms/company-form/company-form.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-company-form',
   templateUrl: './company-form.component.html',
-  styleUrls: ['./company-form.component.css']
+  styleUrls: ['../forms.components.scss']
 })
 export class CompanyFormComponent implements OnInit {
 
--- src/app/features/settings/settings.component.ts
+++ src/app/features/settings/settings.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AppStateService } from '../../services/app-state.service';
-
+import { CompanyFormComponent } from '../forms/company-form/company-form.component';
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit, OnDestroy {
-  subscription: any = null;
-  company: any = null;
+  subscription: any = {};
+  company: any = {};
   params_sub: any = null;
   busy = false;
   constructor(private appState: AppStateService, private route: ActivatedRoute) { }
@@ -17,9 +17,11 @@ export class SettingsComponent implements OnInit, OnDestroy {
   ngOnInit() {
     this.params_sub = this.route.params.subscribe((params) => {
       this.busy = true;
-      // this.subscription = this.appState.subscriptions.find((sub) => sub.alias === params['subscription-alias']);
+      this.appState.getSubscriptionByKey(params['subscription-alias']).subscribe((sub) => {
+        this.subscription = sub;
+        this.busy = false;
+      });
       // this.company = this.subscription.company.clone();
-      this.busy = false;
     });
   }
 
