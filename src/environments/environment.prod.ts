2b8b126d2b1c64bce2e5249c2127b52ab9f93df0
Yiannis Tsenes <itsenes@indice.gr>
2017-11-14 23:11:52 +0200

    added settings for production deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,14 @@
 export const environment = {
   production: true,
-  api_url: 'https://api.incontrl.io'
+  api_url: 'https://api.incontrl.io',
+  auth_settings: {
+    authority: 'https://incontrl.io/',
+    client_id: 'spa',
+    redirect_uri: 'https://app.incontrl.io/auth-callback',
+    post_logout_redirect_uri: 'https://app.incontrl.io/logged-out',
+    response_type: 'id_token token',
+    scope: 'openid profile email core',
+    filterProtocolClaims: true,
+    loadUserInfo: true
+  }
 };
